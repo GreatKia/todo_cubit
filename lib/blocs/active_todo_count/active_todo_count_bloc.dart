1fa2922743fcdb603278871d07e6348c2bc23803
GreatKia <kiarshenpillay@gmail.com>
2023-05-02 21:41:46 +0200

    converted to bloclistener

--- lib/blocs/active_todo_count/active_todo_count_bloc.dart
+++ lib/blocs/active_todo_count/active_todo_count_bloc.dart
@@ -10,35 +10,12 @@ part 'active_todo_count_state.dart';
 
 class ActiveTodoCountBloc
     extends Bloc<ActiveTodoCountEvent, ActiveTodoCountState> {
-  late final StreamSubscription todoListSubscription;
-
   final int initialActiveTodoCount;
-  final TodoListBloc todoListBloc;
   ActiveTodoCountBloc({
     required this.initialActiveTodoCount,
-    required this.todoListBloc,
   }) : super(ActiveTodoCountState(activeTodoCount: initialActiveTodoCount)) {
-    todoListSubscription =
-        todoListBloc.stream.listen((TodoListState todoListState) {
-      print('todoListState: $todoListState');
-
-      final int currentActiveTodoCount = todoListState.todos
-          .where((Todo todo) => !todo.completed)
-          .toList()
-          .length;
-
-      add(CalculateActiveTodoCountEvent(
-          activeTodoCount: currentActiveTodoCount));
-    });
-
     on<CalculateActiveTodoCountEvent>((event, emit) {
       emit(state.copyWith(activeTodoCount: event.activeTodoCount));
     });
-  }
-
-  @override
-  Future<void> close() {
-    todoListSubscription.cancel();
-    return super.close();
   }
 }
